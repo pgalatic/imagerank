c14093e63fca03e5cc54671a5d3fb17b3c00605e
Paul Galatic <pgalatic@gmail.com>
2018-07-25 20:14:42 -0400

    Updated logic, added elo ranking for images
    With Elo, now the ranker will be able to skip comparisons as they become
    "obvious" as denoted by the new COMPARE_FACTOR.
    
    Logic was updated in both files to better reflect their roles (imagerank
    for processing and storing data, gui for displaying it).

--- gui.py
+++ gui.py
@@ -15,22 +15,22 @@ class GUI():
         self._root = tk.Tk()
         self._root.title('Image Ranker')
                 
-        self._filepath = filedialog.askdirectory(
+        self._folder = filedialog.askdirectory(
             parent=self._root,
             initialdir=os.getcwd(),
             title='Please select a folder:'
-        )
+        ) + '/'
         
-        self._ranker = Ranker(self._filepath)
+        if self._folder == '/':
+            sys.exit()
         
-        self._combos = self._ranker.query()
-        self._counter = 1
-        self._var = tk.IntVar()
+        self._ranker = Ranker(self._folder)
         
-        imgA = Image.open(self._filepath + '/' + self._combos[0][0])
+        self._nameA, self._nameB = self._ranker.query()
+        
+        imgA = Image.open(self._folder + self._nameA)
+        imgB = Image.open(self._folder + self._nameB)
         imgA = ImageTk.PhotoImage(imgA.resize(MAXSIZE, Image.ANTIALIAS))
-
-        imgB = Image.open(self._filepath + '/' + self._combos[0][1])
         imgB = ImageTk.PhotoImage(imgB.resize(MAXSIZE, Image.ANTIALIAS))
         
         self._panelA = tk.Label(self._root, image=imgA)
@@ -45,41 +45,29 @@ class GUI():
         self._root.mainloop()
     
     def var_left(self):
-        self._var.set(0)
-        self.nextimage()
+        self._ranker.process(self._nameA, self._nameB)
+        self.update_images()
     
     def var_right(self):
-        self._var.set(1)
-        self.nextimage()
+        self._ranker.process(self._nameB, self._nameA)
+        self.update_images()
         
-    def nextimage(self):
-        if self._counter >= len(self._combos):
-            self._ranker.process(self._scores)
+    def update_images(self):
+        self._nameA, self._nameB = self._ranker.query()
+        if self._nameA is None:
+            print('yes')
+            self._ranker.finish()
             self._root.destroy()
             sys.exit()
         
-        imgA = Image.open(self._filepath + '/' + self._combos[self._counter][0])
+        imgA = Image.open(self._folder + self._nameA)
+        imgB = Image.open(self._folder + self._nameB)
         imgA = ImageTk.PhotoImage(imgA.resize(MAXSIZE, Image.ANTIALIAS))
-        
-        imgB = Image.open(self._filepath + '/' + self._combos[self._counter][1])
         imgB = ImageTk.PhotoImage(imgB.resize(MAXSIZE, Image.ANTIALIAS))
-    
         self._panelA.configure(image=imgA)
         self._panelB.configure(image=imgB)
         self._panelA.image = imgA
         self._panelB.image = imgB
-        
-        if self._var.get() == 0:
-            self._scores.append((1, -1))
-        elif self._var.get() == 1:
-            self._scores.append((-1, 1))
-        else:
-            print(self._var.get())
-            raise Exception('Something went wrong in nextimage()!')
-        
-        self._var.set(-1)
-        
-        self._counter += 1
         
 if __name__ == '__main__':
     gui = GUI()
--- imagerank.py
+++ imagerank.py
@@ -1,34 +1,68 @@
 import os
 import random
 import itertools
+from elo import elo
+from PIL import Image
 
-NUM_FILES = 10
+NUM_FILES = 30
 NUM_PARTITIONS = 5
 DIRNAME = 'ranked/'
+COMPARE_FACTOR = 0.15
 
 class Ranker():
     def __init__(self, folder):
         self._folder = folder + '/'
-        filenames = [f for f in os.listdir(folder + '/.') if not os.path.isdir(f)]
+        filenames = [f for f in os.listdir(folder + '/.') if f.endswith('.png') or f.endswith('.jpg')]
         
         self._chosenfiles = set(random.choices(filenames, k=NUM_FILES))
         self._combinations = list(itertools.combinations(self._chosenfiles, 2))
         self._dist = [int((i+1) * (100 / NUM_PARTITIONS))
                         for i in range(NUM_PARTITIONS)]
+        self._scoredict = {filename: 1000 for filename in self._chosenfiles}
+        self._counter = 0
+        
+        random.shuffle(self._combinations)
     
     def query(self):
-        random.shuffle(self._combinations)
-        return self._combinations
+        valid = False
+        
+        while not valid:
+            if self._counter >= len(self._combinations):
+                return None, None
+        
+            nameA = self._combinations[self._counter][0]
+            nameB = self._combinations[self._counter][1]
+            
+            scoreA = self._scoredict[nameA]
+            scoreB = self._scoredict[nameB]
+            # skip comparisons that are of no contest
+            if abs(elo.expected(scoreA, scoreB) - 0.5) < COMPARE_FACTOR:
+                print(abs(elo.expected(scoreA, scoreB)))
+                valid = True
+            else:
+                print('SKIP: %s vs %s' % (nameA, nameB))
+            
+            self._counter += 1
+            
+        return nameA, nameB
     
-    def process(self, scores):
-        scoredict = {filename: 0 for filename in self._chosenfiles}
+    def process(self, winner, loser):        
+        scoreA = self._scoredict[winner]
+        scoreB = self._scoredict[loser]
         
-        for scorecomp in zip(self._combinations, scores):
-            scoredict[scorecomp[0][0]] += scorecomp[1][0]
-            scoredict[scorecomp[0][1]] += scorecomp[1][1]
+        expA = elo.expected(scoreA, scoreB)
+        expB = elo.expected(scoreB, scoreA)
         
-        sortedscores = sorted(scoredict, key=scoredict.get, reverse=True)
-        l = int(len(sortedscores) / NUM_PARTITIONS)
+        newA = elo.elo(scoreA, expA, 1)
+        newB = elo.elo(scoreB, expB, 0)
+        
+        self._scoredict[winner] = newA
+        self._scoredict[loser] = newB
+                
+    def finish(self):
+        sortedscores = sorted(
+            self._scoredict, key=self._scoredict.get, reverse=True)
+        l = max(int(len(sortedscores) / NUM_PARTITIONS), 1)
         chunks = self.partitions(sortedscores, l)
         
         if not os.path.isdir(self._folder + DIRNAME):
@@ -40,7 +74,7 @@ class Ranker():
                 src = self._folder + f
                 dst = self._folder + DIRNAME + str(obj[1]) + '/' + f
                 os.rename(src, dst)
-        
+                
     def partitions(self, l, n):
         for i in range(0, len(l), n):
             yield l[i:i+n]
